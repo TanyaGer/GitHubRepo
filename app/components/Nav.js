7ef330e653930dacd6a5d0d934ed39f5f7968228
Tania Germanlieva <tania.germanlieva@gmail.com>
2022-05-18 17:18:47 +0200

    Add project

---
+++ app/components/Nav.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { FaRegBell } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+
+import { ThemeConsumer } from '../contexts/Theme';
+
+const activeStyle = {
+  color: 'rgb(187,46,31)',
+};
+
+export default function Nav() {
+  return (
+    <ThemeConsumer>
+      {({ theme, toggleTheme }) => (
+        <nav className='row space-between'>
+          <ul className='row nav'>
+            <li>
+              <NavLink
+                exact
+                to='/'
+                className='nav-link'
+                activeStyle={activeStyle}
+              >
+                Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to='/battle'
+                className='nav-link'
+                activeStyle={activeStyle}
+              >
+                Battle
+              </NavLink>
+            </li>
+          </ul>
+          <button onClick={toggleTheme}>
+            {theme === 'light' ? '🔦' : '💡'}
+          </button>
+        </nav>
+      )}
+    </ThemeConsumer>
+  );
+}
